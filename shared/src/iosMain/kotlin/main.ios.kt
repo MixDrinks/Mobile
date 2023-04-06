0554b29a31b9f1f829cddbbba379861a786abe80
Vova Stelmashchuk <smallstells@gmail.com>
2023-04-06 19:31:44 +0300

    Implement system back button handling

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,5 +1,9 @@
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import org.mixdrinks.app.MixDrinksApp
 
 @Suppress("FunctionNaming")
-fun MainViewController() = ComposeUIViewController { MixDrinksApp() }
+fun MainViewController() = ComposeUIViewController {
+  MixDrinksApp(DefaultComponentContext(LifecycleRegistry()))
+}
