f77a6c35e2d2fdb9a25fcea9b21d235559c2443a
Vova Stelmashchuk <smallstells@gmail.com>
2023-06-19 17:13:35 +0300

    Implement filter count

--- shared/src/commonMain/kotlin/org/mixdrinks/ui/list/main/ListComponent.kt
+++ shared/src/commonMain/kotlin/org/mixdrinks/ui/list/main/ListComponent.kt
@@ -3,12 +3,15 @@ package org.mixdrinks.ui.list.main
 import androidx.compose.ui.text.capitalize
 import androidx.compose.ui.text.intl.Locale
 import com.arkivanov.decompose.ComponentContext
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import org.mixdrinks.data.CocktailsProvider
 import org.mixdrinks.domain.ImageUrlCreators
 import org.mixdrinks.dto.CocktailId
@@ -31,7 +34,17 @@ internal class ListComponent(
         emitAll(cocktailsProvider.getCocktails().map { cocktails ->
             map(cocktails)
         })
-    }.flowOn(Dispatchers.Default).stateInWhileSubscribe()
+    }
+        .flowOn(Dispatchers.Default)
+        .stateInWhileSubscribe()
+
+    val filterCountState: StateFlow<Int?> = mutableFilterStorage.selected
+        .map { filterGroups ->
+            filterGroups.flatMap { it.value }.count().takeIf { it > 0 }
+        }
+        .stateIn(
+            CoroutineScope(Dispatchers.Main), SharingStarted.WhileSubscribed(), null
+        )
 
     private suspend fun map(cocktails: List<CocktailsProvider.Cocktail>): UiState.Data<CocktailsListState> {
         return UiState.Data(
