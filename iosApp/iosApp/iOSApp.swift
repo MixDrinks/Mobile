8c12a98ae70b11fc66ef3d7ba74be12ca9fe82d2
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2023-07-01 19:39:52 +0300

    add export option for production ios build

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -4,18 +4,18 @@ import GoogleSignIn
 import AuthenticationServices
 import UIKit
 
-class AppDelegate: NSObject, UIApplicationDelegate{
+class AppDelegate: NSObject, UIApplicationDelegate {
 
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-      FirebaseApp.configure()
-    return true
-  }
-    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+
     func application(_ app: UIApplication,
                      open url: URL,
                      options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-      return GIDSignIn.sharedInstance.handle(url)
+        return GIDSignIn.sharedInstance.handle(url)
     }
 
 }
@@ -23,10 +23,10 @@ class AppDelegate: NSObject, UIApplicationDelegate{
 @main
 struct iOSApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
 }
