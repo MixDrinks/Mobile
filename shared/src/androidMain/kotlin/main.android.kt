6de67e5b3467b98c7f62e3f2b7a15c9819d9e235
Volodymyr Stelmashchuk <smallstells@gmail.com>
2023-07-03 16:49:10 +0300

    fix test fly build (#84)
    * Fixe deploy to test fly
    
    * fix yaml
    
    * Implemet back handle for ios
    
    * fix test ci
    
    * migrate to java 17

--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -1,7 +1,10 @@
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.decompose.defaultComponentContext
+import com.arkivanov.essenty.backhandler.BackDispatcher
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import org.mixdrinks.app.MixDrinksApp
 import org.mixdrinks.di.GraphHolder
 import org.mixdrinks.ui.auth.AuthCallbacks
