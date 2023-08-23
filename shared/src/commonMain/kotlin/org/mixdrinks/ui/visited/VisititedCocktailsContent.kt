629e431320a2f58d52a77eb32fd1c6c3d07c9c13
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2023-08-23 09:47:33 +0300

    Add logs to update logs

--- shared/src/commonMain/kotlin/org/mixdrinks/ui/visited/VisititedCocktailsContent.kt
+++ shared/src/commonMain/kotlin/org/mixdrinks/ui/visited/VisititedCocktailsContent.kt
@@ -1,12 +1,8 @@
 package org.mixdrinks.ui.visited
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
@@ -15,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import org.mixdrinks.app.styles.MixDrinksColors
 import org.mixdrinks.app.styles.MixDrinksTextStyles
 import org.mixdrinks.app.utils.ResString
 import org.mixdrinks.ui.list.cocktailListInserter
